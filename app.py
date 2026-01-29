84e766b07dafb0683823737d40ca8ed25b90e3b6
Mikhail <MoudClam@users.noreply.huggingface.co>
2026-01-29 13:20:47 +0000

    Update app.py
    get_current_time_in_timezone added to CodeAgent.

--- app.py
+++ app.py
@@ -55,7 +55,10 @@ with open("prompts.yaml", 'r') as stream:
     
 agent = CodeAgent(
     model=model,
-    tools=[final_answer], ## add your tools here (don't remove final answer)
+    tools=[
+        final_answer,
+        get_current_time_in_timezone,
+    ],
     max_steps=6,
     verbosity_level=1,
     grammar=None,
